0904bf53e0d45a3db781fcee9d5888260416e767
Drogaria Cidade <informatica@drogcidade.com.br>
2026-04-29 08:16:18 -0300

    Update download_table.py

--- scripts/download_table.py
+++ scripts/download_table.py
@@ -90,8 +90,8 @@ try:
     time.sleep(5)
 
     # Fill in start and end dates
-    driver.find_element(By.ID, "dat_inicio").send_keys("01062025")
-    driver.find_element(By.ID, "dat_fim").send_keys("31072025")
+    driver.find_element(By.ID, "dat_inicio").send_keys("01082025")
+    driver.find_element(By.ID, "dat_fim").send_keys("30092025")
     time.sleep(5)
 
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "selI_1"))).click()
